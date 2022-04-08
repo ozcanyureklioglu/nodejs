bef716cbb2ae919ac984b652f99e9552f8664953
Özcan Yüreklioğlu <ozcan.yu@hotmail.com>
2022-04-08 20:44:54 +0300

    fs module

---
+++ fs.js
@@ -0,0 +1,37 @@
+
+var fs = require('fs');
+
+
+
+/*
+fs.readFile('./password.txt', 'utf8', (err, data) => {
+
+    if (err) console.log(err);
+    else {
+        console.log(data);
+        console.log("Dosya Okuma Başarılı");
+    }
+});
+
+fs.writeFile('user.json','{"name": "Özcan","surname":"Yureklioğlu"}','utf8',(err)=>{
+
+    if(err) console.log(err);
+    else{
+        console.log("Dosya Oluşturma Başarılı");
+    }
+});
+
+
+
+fs.appendFile('password.txt','\nDENEME 222','utf8',(err)=>{
+    if(err) console.log(err);
+});
+
+*/
+
+
+
+
+
+
+
